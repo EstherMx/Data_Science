fcf550d3b975043984f0b85e8f211c277ebe9393
EstherMx <esmaxi21@gmail.com>
2022-07-02 20:47:17 -0400

    parts of the MotherBoard

---
+++ python/motherBoard.py
@@ -0,0 +1,29 @@
+To open Python on the command line, type 'py'. To exit, write 'exit()'
+
+
+print ('hello from sublime file e2');
+
+rasberry:
+the software contains the CPU and main memory
+central processing unit: ask 'what to do next?' is fast
+main or primary memory: fast small temporary storage- ie: RAM(Random Access Memory), which is primary-volatile memory
+ie: ROM(Read Only Memory) is a primary-non volatile memory. When they are running, programs are stored in the main memory
+
+secondary memory: slower large permanent storage - disk drive/ memory stick, ie: HDD(Hard Disk Drive), SSD(Solid State Drive) and RAM ()
+input devices:keyboard, mouse, touch screen
+ output devices: screen, speakers, printer
+
+the brain of the computer is the central processing unit with pins 
+64 pins asks what we want to do.print
+
+All the instructions are kept in the main memory
+which feeds the CPU with instructors. 
+
+
+the 2nd memory will save everything even when the power is off.
+
+capacitor, resistor and transitors are tinier on a microprocessor (r: image)
+
+ ROM are usually hard-coded into the hardware itself, and cannot be changed or deleted. As for HDD and SSD, these are just storage devices used to store user and OS data, like movies, music, Windows files, etc. ROM is usually not read even by the OS, it's a very low-level storage which can't be easily accessed.
+
+ _source: motherboard explained video:https://www.youtube.com/watch?v=b2pd3Y6aBag
