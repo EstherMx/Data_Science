3a34faef19d10e7258b5ae56e6140ead3cc5ee2c
EstherMx <esmaxi21@gmail.com>
2022-07-03 17:37:20 -0400

    overtime pay rate

--- python/pyExercises.py
+++ python/pyExercises.py
@@ -2,3 +2,10 @@ hours = input("How many hours have you worked?")
 rate = input("How much are you paid an hour?")
 pay = int(hours) * int(rate)
 print('Pay:', pay)
+
+# Rewrite your pay computation to give 1.5 times the hourly rate for hours worked above
+# 40 hours.
+if int(hours) > 40:
+	overTime = int(hours) - 40
+	overTime *= 1.5 * int(rate)
+print ('OverTime pay:', overTime)
