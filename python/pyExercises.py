6f70fd8110106c403e16e32993abc15dba19a991
EstherMx <esmaxi21@gmail.com>
2022-07-03 21:15:19 -0400

    ie using try and except. used quit() as a return for the whole code

--- python/pyExercises.py
+++ python/pyExercises.py
@@ -1,6 +1,15 @@
-hours = input("How many hours have you worked?")
-rate = input("How much are you paid an hour?")
-pay = int(hours) * int(rate)
+try:
+	hours = input("How many hours have you worked?")
+	rate = input("How much are you paid an hour?")
+except:
+	print('this is not a number')
+try:
+	pay = int(hours) * int(rate)
+except: 
+	print('enter a number')	
+	quit()
+
+
 print('Pay:', pay)
 
 # Rewrite your pay computation to give 1.5 times the hourly rate for hours worked above
@@ -9,3 +18,4 @@ if int(hours) > 40:
 	overTime = int(hours) - 40
 	overTime *= 1.5 * int(rate)
 print ('OverTime pay:', overTime)
+
