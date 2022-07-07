977573f5997c9d6594ecd6efac639e6470a85547
EstherMx <esmaxi21@gmail.com>
2022-07-06 23:34:25 -0400

    modified the DS roadmap and added a ie to find the smallest number of an array

--- python/pyExercises.py
+++ python/pyExercises.py
@@ -54,3 +54,15 @@ while True:
 # 1 to n and this new n is pushed back up to the while loop 
 # until n = 3 and hits the break, which is the end of the loop. NO parentheses
 # no curly brakets, no semicolon
+
+
+
+# Find the smallest value from a list of values.
+
+smallest = None
+print('Before:', smallest)
+for itervar in [3, 41,12,9,74,15]:
+	if smallest is None or itervar < smallest:
+		smallest = itervar
+	print('Loop:', itervar, smallest)
+print('Smallest number:', smallest)
